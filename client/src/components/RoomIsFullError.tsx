85254285c96b218c046a2d401ad1b3a759a1c41d
Aleksandra Łapczuk <sandralapcz@gmail.com>
2023-07-11 14:43:42 +0200

    room is full error

--- client/src/components/RoomIsFullError.tsx
+++ client/src/components/RoomIsFullError.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import {Headline2} from './App.styles';
+import {Copy, Headline2} from './App.styles';
 import doNotEnter from '../assets/Do not enter sign-pana.png';
+import {Link} from "react-router-dom";
 
 const RoomIsFullError = () => {
 
-	return <div
-		style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', overflowY: "hidden"}}>
-		<span style={{position: 'absolute', top: 0}}>This room is already full :). Choose another one</span>
-		<img src={doNotEnter} alt={"STOP! THIS ROOM IS FULL"} style={{height: "85vh"}}/>
-		{/*<Headline2 style={{alignItems:"center"}}>This room is full!</Headline2>*/}
-		<a style={{position: 'absolute', bottom: 0}} href="https://storyset.com/data">Data illustrations by Storyset</a>
+	return <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', height: '97vh', overflowY: 'hidden' }}>
+		<Headline2 style={{ marginRight: '10px' }}>This room is already full.</Headline2>
+		<Copy style={{marginTop:"5px"}}>
+			<Link to="/" style={{ textDecoration: 'none' }}>Back to homepage</Link>
+		</Copy>
+		<img src={doNotEnter} alt="STOP! THIS ROOM IS FULL" style={{ height: '85vh' }} />
 	</div>
 };
-
 export default RoomIsFullError;
