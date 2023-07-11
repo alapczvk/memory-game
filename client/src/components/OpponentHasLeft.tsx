dcfe3caea594a995d705e692373647b135ed9558
Aleksandra Łapczuk <sandralapcz@gmail.com>
2023-07-11 19:15:14 +0200

    opponent left

--- client/src/components/OpponentHasLeft.tsx
+++ client/src/components/OpponentHasLeft.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import logo from "../assets/logo.png";
+import {Copy, Headline2} from "./App.styles";
 
 const OpponentHasLeft = () => {
-	return <div>Opponent has left - refresh page to start game once again</div>;
-};
+    return (<div style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "90vh",
+        overflowY: "hidden"
+    }}>
+        <img
+            src={logo}
+            alt="Logo"
+            style={{
+                display: "block",
+                margin: "0 auto",
+                marginBottom: "16px",
+                animation: "logo-animation 3.5s linear infinite"
+            }}
+            className="logo-animation"
+        />
+        <Headline2 style={{alignItems: "center"}}>Your opponent left.</Headline2>
+            <Copy onClick={() => window.location.reload()}>Refresh page</Copy>
+        </div>
+);
+}
+;
 
 export default OpponentHasLeft;
